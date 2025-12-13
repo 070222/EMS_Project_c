e0de7dbf2b0d352cd19dc13fdab6b5bf4d843c89
小黑 <wangjj0702@outlook.com>
2025-12-13 12:33:52 +0800

    与用户交互模块

--- main.c
+++ main.c
@@ -3,7 +3,41 @@ void menu();
 
 int main()
 {
-	menu();
+	while (1)
+	{
+		int choice;
+		menu();
+		scanf_s("%d", &choice);
+		switch (choice)
+		{
+		case 1:
+			printf("添加成功\n");
+			break;
+		case 2:
+			printf("删除成功\n");
+			break;
+		case 3:
+			printf("修改成功\n");
+			break;
+		case 4:
+			printf("查询成功\n");
+			break;
+		case 5:
+		    printf("排序成功\n");
+			break;
+		case 6:
+			printf("保存成功\n");
+			break;
+		case 0:
+			printf("\n\n感谢您的使用\n\n");
+			exit(0);
+		default:
+			printf("输入有误，请重新输入\n");
+			system("pause");
+			break;
+		}
+	}
+	
 	return 0;
 }
 void menu()
